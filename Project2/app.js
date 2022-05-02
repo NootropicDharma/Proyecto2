b76df73c3d69ccbe11b515379bfdde6a8ba31695
Emmanuel Alejandro Borrayo Cortes <alexborrayo21@hotmail.com>
2022-05-02 18:00:30 -0500

    Vista Login Signup

--- Project2/app.js
+++ Project2/app.js
@@ -28,6 +28,7 @@ const index = require("./routes/index.routes");
 app.use("/", index);
 
 const authRoutes = require("./routes/auth.routes");
+const { geoSearch } = require("./models/User.model");
 app.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
