d3b8089ec782abd414be25cf46c1e463e264a378
David Garcia <metaphysicaless1489@gmail.com>
2022-05-02 18:46:00 -0600

    update

--- Project2/routes/profile.routes.js
+++ Project2/routes/profile.routes.js
@@ -1,6 +1,5 @@
-const { post } = require('spotify-web-api-node/src/http-manager');
 const Event = require('../models/evento.model');
-
+const User = require('../models/User.model')
 const router = require('express').Router();
 
 router.get('/profile', (req, res)=>{
@@ -8,6 +7,25 @@ router.get('/profile', (req, res)=>{
         res.render('../views/profile/userProfile.hbs')
 
 })
+
+// router.get('/profile', (req, res)=>{
+//         User.find({req.session.})
+//         .then()
+//         const {name, email , Avatar, username} = req.body  
+        
+// })
+
+
+
+// router.get("/profile", (req, res) => {
+//         Event.find()
+//         .then(data => {
+//                 res.render("list of events", {data})
+//         })
+//         .catch(console.log)
+// })
+
+
 
 module.exports = router;
 
